db24334d794a5f3b78445a86807454c6075708ea
Kevin Lee <klee.taurus@gmail.com>
2023-04-18 20:52:48 +0800

    support read srt file online

--- summary.go
+++ summary.go
@@ -15,11 +15,32 @@ const (
 	MaxTokensPerRequest = 2048
 )
 
-func SummaryFile(prompt, filename string) (string, error) {
-	textChunks := ReadTextFile(filename)
+var (
+	prompt = "`reset` " +
+		"`no quotes` " +
+		"`no explanations` " +
+		"`no prompt` " +
+		"`no self-reference` " +
+		"`no apologies` " +
+		"`no filler` " +
+		"`just answer` " + `
+I will give you text content, you will rewrite it and output that in a short 
+summarized version of my text. Keep the meaning the same. Ensure that the 
+revised content has significantly fewer characters than the original text, 
+and no more than 500 Chinese words, the fewer the better.
+` +
+		`When generating text summaries, expand around the following topics as
+much as possible: %s` +
+		`Only give me the output and nothing else. Now, using the concepts above, 
+summarize the following text. Respond in Chinese language:
+`
+)
+
+func SummaryFile(customPrompt, filename string) (string, error) {
+	textChunks := ReadSRTFile(filename)
 
 	fmt.Println("Total chunks of input file: ", len(textChunks))
-	summaryChunks, err := RecursiveSummary(prompt, textChunks, BaseChunkDepth)
+	summaryChunks, err := RecursiveSummary(fmt.Sprintf(prompt, customPrompt), textChunks, BaseChunkDepth)
 	if err != nil {
 		return "", err
 	}
